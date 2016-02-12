40e3276ae5bd7bfcef6d6806b0e8de1a8ee0108f
reprap <reprap@reprap-ThinkPad-T60.(none)>
2016-02-12 22:24:15 +0100

    smaller stepper with half of the teeth

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -487,7 +487,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {43.956,43.956,200.0*8/3*2.5,760*1.1*2.5*0.834}  // default steps per unit for RepMan3.2
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {43.956,43.956,200.0*8/3*2.5,760*1.1*2.5*0.834 * 2}  // default steps per unit for RepMan3.2
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
